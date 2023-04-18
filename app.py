5e953d374f27d38bda217c036fe9bdcd669fb4c6
Gustavo Chavez <104087273+gusadrcha@users.noreply.github.com>
2023-04-17 20:20:30 -0500

    added in an icon for the application

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ width = LMS_GUI_WINDOW.winfo_screenwidth()/2
 
 LMS_GUI_WINDOW.geometry(f'{int(width)}x{int(height)}')
 
+# change the icon of the application
+LMS_GUI_WINDOW.iconbitmap("./book-icon-file-13.jpg")
+
 # set the title of the GUI
 LMS_GUI_WINDOW.title("Library Management System")
 
