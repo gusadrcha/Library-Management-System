a2ce0b9c507ca107d44b56ad818eb9c806bbde12
AbrahamMookhoek <abraham.mookhoek@mavs.uta.edu>
2023-04-26 22:36:55 -0500

    implemented query1 -AJ

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import Calendar, DateEntry
+from datetime import date, timedelta
 import sqlite3
 
 _white = "#ffffff"
@@ -13,9 +14,6 @@ _skyblue = "#00a8e8"
 LMS_GUI_WINDOW = tk.Tk()
 
 LMS_GUI_WINDOW.config(bg=_black)
-
-# connects the GUI with the LMS database
-connection = sqlite3.connect("LMS.db")
 
 # default size of window when first created
 height = LMS_GUI_WINDOW.winfo_screenheight()
@@ -49,49 +47,105 @@ tabControl.pack(expand=1, fill='both')
 
 # -- Query 1 --
 # book input frame that holds all book entries-----------------
+def Q1Submit():
+    Q1Connection = sqlite3.connect("LMS.db")
+    Q1Cursor = Q1Connection.cursor()
+
+    Q1Cursor.execute("SELECT * FROM Book WHERE Book_id = ?",(bookIDEntry.get(),))
+    tempBook = Q1Cursor.fetchall()
+    if(tempBook):
+        Q1Cursor.execute("SELECT * FROM Library_branch WHERE Branch_id = ?",(libraryBranchIDEntry.get(),))
+        tempBranch = Q1Cursor.fetchall()
+        if(tempBranch):
+            Q1Cursor.execute("SELECT * FROM Borrower WHERE Card_no = ?",(bookBorrowerEntry.get(),))
+            tempBorrower = Q1Cursor.fetchall()
+            if(tempBorrower):
+                Q1Cursor.execute("SELECT No_of_copies FROM Book_copies WHERE Book_id = ? AND Branch_id = ?",
+                                 (tempBook[0][0],tempBranch[0][0],))
+                tempCopyNum = Q1Cursor.fetchall()
+                if(int(tempCopyNum[0][0]) > 0):
+                    Q1Cursor.execute("INSERT INTO Book_Loans VALUES (?,?,?,?,?,?)",
+                                    (bookIDEntry.get(),libraryBranchIDEntry.get(),bookBorrowerEntry.get(),date.today(),date.today()+timedelta(days=30),None,))
+                    Q1Cursor.execute("UPDATE Book_copies SET No_of_copies = No_of_copies-1 WHERE Book_id = ? AND Branch_id = ?",
+                                    (tempBook[0][0],tempBranch[0][0],))
+                    Q1Cursor.execute("SELECT No_of_copies FROM Book_copies WHERE Book_id = ? AND Branch_id = ?",
+                                    (tempBook[0][0],tempBranch[0][0],))
+                    results = Q1Cursor.fetchall()
+                    resultsLabel = tk.Label(bookInputFrame1, text=results[0]).grid(row=1, column=2, columnspan=2)
+                else:
+                    Q1error4 = tk.Label(bookInputFrame1, text="0 Copies at Specified Branch").grid(row=1, column=2, columnspan=2)
+            else:
+                Q1error3 = tk.Label(bookInputFrame1, text="Invalid Card No.").grid(row=1, column=2, columnspan=2)
+        else:
+            Q1error2 = tk.Label(bookInputFrame1, text="Invalid Branch ID").grid(row=1, column=2, columnspan=2)
+    else:
+        Q1error1 = tk.Label(bookInputFrame1, text="Invalid Book ID").grid(row=1, column=2, columnspan=2)
+
+
+    Q1Connection.commit()
+    Q1Connection.close()
+
+def Q1InputQuery():
+    pass
+
 bookInputFrame1 = tk.Frame(tab1, background="white")
 bookInputFrame1.grid(row=0, column=0, padx=20, pady=20)
 
 # T = Text(root, bg, fg, bd, height, width, font, ..)
 Q1DescriptionLabel = tk.Label(bookInputFrame1,
-                              text="Check Out a Book").grid(row=0, column=0, columnspan=4, pady=10)
+                              text="Check Out a Book").grid(row=0, column=0, columnspan=2, padx=5, pady=10)
 
-bookNameEntry = tk.Entry(bookInputFrame1)
-bookNameEntryLabel = tk.Label(bookInputFrame1, text="Book Title")
-bookNameEntryLabel.grid(row=1, column=0, sticky='sw')
-bookNameEntry.grid(row=2, column=0, columnspan=2)
+Q1OutputLabel = tk.Label(bookInputFrame1,
+                              text="Output").grid(row=0, column=2, columnspan=2, padx=5, pady=10)
+
+# bookNameEntry = tk.Entry(bookInputFrame1)
+# bookNameEntryLabel = tk.Label(bookInputFrame1, text="Book Title")
+# bookNameEntryLabel.grid(row=1, column=0, sticky='sw')
+# bookNameEntry.grid(row=2, column=0, columnspan=2)
 
 bookIDEntry = tk.Entry(bookInputFrame1)
 bookIDEntryLabel = tk.Label(bookInputFrame1, text="Book ID")
-bookIDEntryLabel.grid(row=1, column=2, sticky="sw")
-bookIDEntry.grid(row=2, column=2, columnspan=2)
+bookIDEntryLabel.grid(row=1, column=0, columnspan=2, sticky="sw")
+bookIDEntry.grid(row=2, column=0, columnspan=2)
 
-bookPublisherEntry = tk.Entry(bookInputFrame1)
-bookPublisherEntryLabel = tk.Label(bookInputFrame1, text="Book Publisher")
-bookPublisherEntryLabel.grid(row=3, column=0, sticky="sw")
-bookPublisherEntry.grid(row=4, column=0, columnspan=2)
+libraryBranchIDEntry = tk.Entry(bookInputFrame1)
+libraryBranchIDEntryLabel = tk.Label(bookInputFrame1, text="Branch ID")
+libraryBranchIDEntryLabel.grid(row=3, column=0, columnspan=2, sticky="sw")
+libraryBranchIDEntry.grid(row=4, column=0, columnspan=2)
 
 bookBorrowerEntry = tk.Entry(bookInputFrame1)
 bookBorrowerEntryLabel = tk.Label(bookInputFrame1, text="Card No.")
-bookBorrowerEntryLabel.grid(row=3, column=2, sticky="sw")
-bookBorrowerEntry.grid(row=4, column=2, columnspan=2)
+bookBorrowerEntryLabel.grid(row=5, column=0, columnspan=2, sticky="sw")
+bookBorrowerEntry.grid(row=6, column=0, columnspan=2)
 
 Q1SubmitButton = tk.Button(bookInputFrame1, text = 'Submit',
-                           command = 1+1).grid(row=5, column=0,columnspan=4, pady=10)
+                           command = Q1Submit).grid(row=7, column=0,columnspan=2, pady=10)
 # ----------------------------------
 
 # -- Query 2 --
 # borrower input frame that holds all borrower entries --------------
+def Q2Submit():
+    Q2Connection = sqlite3.connect("LMS.db")
+    Q2Cursor = Q2Connection.cursor()
+
+    Q2Cursor.execute("")
+
+    Q2Connection.commit()
+    Q2Connection.close()
+
+def Q2InputQuery():
+    pass
+
 borrowerInputFrame = tk.Frame(tab2, background="white")
 borrowerInputFrame.grid(row=0, column=0, padx=20, pady=20)
 
 Q2DescriptionLabel = tk.Label(borrowerInputFrame,
                               text="Create Library Account").grid(row=0, column=0, columnspan=2, pady=10)
 
-borrwerNameEntry = tk.Entry(borrowerInputFrame)
-borrwerNameEntryLabel = tk.Label(borrowerInputFrame, text="Borrower Name")
-borrwerNameEntryLabel.grid(row=1, column=0, columnspan=2, sticky="sw")
-borrwerNameEntry.grid(row=2, column=0, columnspan=2)
+borrowerNameEntry = tk.Entry(borrowerInputFrame)
+borrowerNameEntryLabel = tk.Label(borrowerInputFrame, text="Borrower Name")
+borrowerNameEntryLabel.grid(row=1, column=0, columnspan=2, sticky="sw")
+borrowerNameEntry.grid(row=2, column=0, columnspan=2)
 
 borrowerAddressEntry = tk.Entry(borrowerInputFrame)
 borrowerAddressEntryLabel = tk.Label(borrowerInputFrame, text="Borrower Address")
@@ -104,25 +158,37 @@ borrowerPhoneNumberLabel.grid(row=5, column=0, columnspan=2, sticky="sw")
 borrowerPhoneNumberEntry.grid(row=6, column=0, columnspan=2)
 
 Q2SubmitButton = tk.Button(borrowerInputFrame, text = 'Submit',
-                           command = 1+1).grid(row=7, column=0,columnspan=2, pady=10)
+                           command = Q2Submit).grid(row=7, column=0,columnspan=2, pady=10)
 #------------------------------
 
 #-- Query 3 --
+def Q3Submit():
+    Q3Connection = sqlite3.connect("LMS.db")
+    Q3Cursor = Q3Connection.cursor()
+
+    Q3Cursor.execute("")
+
+    Q3Connection.commit()
+    Q3Connection.close()
+
+def Q3InputQuery():
+    pass
+
 bookInputFrame2 = tk.Frame(tab3, background="white")
 bookInputFrame2.grid(row=0, column=0, padx=20, pady=20)
 
 Q3DescriptionLabel = tk.Label(bookInputFrame2,
                               text="Add Book to System").grid(row=0, column=0, columnspan=2, pady=10)
 
-bookNameEntry = tk.Entry(bookInputFrame2)
-bookNameEntryLabel = tk.Label(bookInputFrame2, text="Book Title")
-bookNameEntryLabel.grid(row=1, column=0, columnspan=2, sticky='sw')
-bookNameEntry.grid(row=2, column=0, columnspan=2)
+Q3bookTitleEntry = tk.Entry(bookInputFrame2)
+Q3bookTitleEntryLabel = tk.Label(bookInputFrame2, text="Book Title")
+Q3bookTitleEntryLabel.grid(row=1, column=0, columnspan=2, sticky='sw')
+Q3bookTitleEntry.grid(row=2, column=0, columnspan=2)
 
-# bookIDEntry = tk.Entry(bookInputFrame2)
-# bookIDEntryLabel = tk.Label(bookInputFrame2, text="Book ID")
-# bookIDEntryLabel.grid(row=1, column=2, columnspan=2, sticky="sw")
-# bookIDEntry.grid(row=2, column=2, columnspan=2)
+# Q3bookIDEntry = tk.Entry(bookInputFrame2)
+# Q3bookIDEntryLabel = tk.Label(bookInputFrame2, text="Book ID")
+# Q3bookIDEntryLabel.grid(row=1, column=2, columnspan=2, sticky="sw")
+# Q3bookIDEntry.grid(row=2, column=2, columnspan=2)
 
 bookPublisherEntry = tk.Entry(bookInputFrame2)
 bookPublisherEntryLabel = tk.Label(bookInputFrame2, text="Book Publisher")
@@ -135,24 +201,48 @@ bookAuthorEntryLabel.grid(row=5, column=0, columnspan=2, sticky="sw")
 bookAuthorEntry.grid(row=6, column=0, columnspan=2)
 
 Q3SubmitButton = tk.Button(bookInputFrame2, text = 'Submit',
-                           command = 1+1).grid(row=7, column=0,columnspan=2, pady=10)
+                           command = Q3Submit).grid(row=7, column=0,columnspan=2, pady=10)
 
 # -- Query 4 --
+def Q4Submit():
+    Q4Connection = sqlite3.connect("LMS.db")
+    Q4Cursor = Q4Connection.cursor()
+
+    Q4Cursor.execute("")
+
+    Q4Connection.commit()
+    Q4Connection.close()
+
+def Q4InputQuery():
+    pass
+
 bookCopiesPerBranchInputFrame = tk.Frame(tab4, background="white")
 bookCopiesPerBranchInputFrame.grid(row=0, column=0, padx=20, pady=20)
 
 Q4DescriptionLabel = tk.Label(bookCopiesPerBranchInputFrame,
                               text="Search for a Book").grid(row=0, column=0, columnspan=2, pady=10)
 
-bookTitleEntry = tk.Entry(bookCopiesPerBranchInputFrame)
-bookTitleEntryLabel = tk.Label(bookCopiesPerBranchInputFrame, text="Book Title")
-bookTitleEntryLabel.grid(row=1, column=0, columnspan=2, sticky="sw")
-bookTitleEntry.grid(row=2, column=0, columnspan=2)
+Q4bookTitleEntry = tk.Entry(bookCopiesPerBranchInputFrame)
+Q4bookTitleEntryLabel = tk.Label(bookCopiesPerBranchInputFrame, text="Book Title")
+Q4bookTitleEntryLabel.grid(row=1, column=0, columnspan=2, sticky="sw")
+Q4bookTitleEntry.grid(row=2, column=0, columnspan=2)
 
 Q4SubmitButton = tk.Button(bookCopiesPerBranchInputFrame, text = 'Submit',
-                           command = 1+1).grid(row=7, column=0,columnspan=2, pady=10)
+                           command = Q4Submit).grid(row=7, column=0,columnspan=2, pady=10)
 
 # -- Query 5 --
+def Q5Submit():
+    Q5Connection = sqlite3.connect("LMS.db")
+    Q5Cursor = Q5Connection.cursor()
+
+    Q5Cursor.execute("")
+
+    Q5Connection.commit()
+    Q5Connection.close()
+
+def Q5InputQuery():
+    pass
+
 dueDatesInputFrame = tk.Frame(tab5, background="white")
 dueDatesInputFrame.grid(row=0, column=0, padx=20, pady=20)
 
@@ -168,7 +258,7 @@ cal2 = DateEntry(dueDatesInputFrame, width= 16, background= "magenta3", foregrou
 cal2.grid(row=4, column=0, columnspan=2, sticky="sw")
 
 Q5SubmitButton = tk.Button(dueDatesInputFrame, text = 'Submit',
-                           command = 1+1).grid(row=5, column=0,columnspan=2, pady=10)
+                           command = Q5Submit).grid(row=5, column=0,columnspan=2, pady=10)
 
 # main_frame = tk.Frame(LMS_GUI_WINDOW)
 # main_frame.pack(fill=tk.BOTH, expand=1)
@@ -288,6 +378,3 @@ Q5SubmitButton = tk.Button(dueDatesInputFrame, text = 'Submit',
 
 # creates the mainloop of the GUI
 LMS_GUI_WINDOW.mainloop()
-
-# closes the connection to the database
-connection.close()
